d2e3c0e2b157fcbd1a7043860c5391f8467fbabb
NurAkbarr <setyaakbar33@gmail.com>
2026-04-12 04:42:35 +0700

    Update favicon

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - Matla Backend</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/logo-bulat.png') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/logo-bulat.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('assets/logo-bulat.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
