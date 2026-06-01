ccc5fb6457b87d36f76136b48187082f95bf0b77
NurAkbarr <setyaakbar33@gmail.com>
2026-06-01 11:01:23 +0700

    Update Kotak Pesan5

--- resources/views/backend/admin/messages/index.blade.php
+++ resources/views/backend/admin/messages/index.blade.php
@@ -58,14 +58,14 @@
                                             <div x-show="showReply" 
                                                  x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" 
                                                  x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" 
-                                                 @click="showReply = false" class="fixed inset-0 bg-gray-900 bg-opacity-50 transition-opacity" aria-hidden="true"></div>
+                                                 @click="showReply = false" class="absolute inset-0 bg-gray-900 bg-opacity-50 transition-opacity" aria-hidden="true"></div>
                                             
                                             <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
                                             
                                             <div x-show="showReply" 
                                                  x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" 
                                                  x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" 
-                                                 class="inline-block align-bottom bg-white rounded-3xl text-left overflow-hidden shadow-2xl transform transition-all sm:my-8 sm:align-middle sm:max-w-2xl sm:w-full border border-gray-100">
+                                                 class="relative z-10 inline-block align-bottom bg-white rounded-3xl text-left overflow-hidden shadow-2xl transform transition-all sm:my-8 sm:align-middle sm:max-w-2xl sm:w-full border border-gray-100">
                                                 
                                                 <form action="{{ route('backend.admin.messages.reply', $msg) }}" method="POST">
                                                     @csrf
